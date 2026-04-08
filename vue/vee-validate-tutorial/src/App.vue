a4e16511463ab5837b78a0a2e29979f663ab220b
mooyah5 <qorgkssk123@gmail.com>
2026-04-08 20:20:20 +0900

    feat(vee-validate-tutorial): add zod, validation schema

--- vue/vee-validate-tutorial/src/App.vue
+++ vue/vee-validate-tutorial/src/App.vue
@@ -1,29 +1,36 @@
 <template>
-  <div id="app">
-    <Form @submit="onSubmit">
-      <Field name="email" type="email" :rules="validateEmail" />
-      <ErrorMessage name="email" />
-      <button>Sign up</button>
-    </Form>
-  </div>
+  <Form :initial-values="INITIAL_VALUES" :validation-schema="schema" @submit="onSubmit">
+    <label for="email">email</label>
+    <Field id="email" name="email" type="email" />
+    <ErrorMessage name="email" />
+    <br />
+    <label for="name">name </label>
+    <Field id="name" name="name" />
+    <ErrorMessage name="name" />
+
+    <br />
+    <button>Submit</button>
+  </Form>
 </template>
 
 <script setup>
 import { Form, Field, ErrorMessage } from "vee-validate"
 
-const validateEmail = (value) => {
-  // if the field is empty
-  if (!value) {
-    return "This field is required"
-  }
-  // if the field is not a valid email
-  const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
-  if (!regex.test(value)) {
-    return "This field must be a valid email"
-  }
-  // All is good
-  return true
+import { toTypedSchema } from "@vee-validate/zod"
+import { z } from "zod"
+
+const INITIAL_VALUES = {
+  email: "",
+  name: ""
 }
+
+const schema = toTypedSchema(
+  z.object({
+    email: z.string().trim().min(1, "이메일을 입력해주세요").email("올바른 이메일 형식이 아닙니다"),
+    name: z.string().trim().min(1, "이름을 입력해주세요")
+  })
+)
+
 const onSubmit = (values) => {
   alert(JSON.stringify(values, null, 2))
 }
